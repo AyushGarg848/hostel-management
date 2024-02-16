d14b40c4107d6e6369a8140a0d90dbc7efe7779e
Ayush Garg <ayushkumargarg848@gmail.com>
2024-02-17 01:10:21 +0530

    Modified Contact us

--- contact.php
+++ contact.php
@@ -84,40 +84,19 @@
 <section class="contact py-5">
 	<div class="container">
 		<h2 class="heading text-capitalize mb-sm-5 mb-4"> Contact Us </h2>
-			<div class="mail_grid_w3l">
-				<form action="contact.php" method="post">
-					<div class="row">
-						<div class="col-md-6 contact_left_grid" data-aos="fade-right">
-							<div class="contact-fields-w3ls">
-								<input type="text" name="hostel_name" placeholder="Hostel Name" required="">
-							</div>
-							<div class="contact-fields-w3ls">
-								<input type="text" name="name" placeholder="Name" value="<?php echo $_SESSION['fname']." ".$_SESSION['lname']; ?>" required="">
-							</div>
-							<div class="contact-fields-w3ls">
-								<input type="text" name="rol_no" placeholder="Roll Number" value="<?php echo $_SESSION['roll']; ?>" required="">
-							</div>
-							<div class="contact-fields-w3ls">
-								<input type="text" name="subject" placeholder="Subject" required="">
-							</div>
-						</div>
-						<div class="col-md-6 contact_left_grid" data-aos="fade-left">
-							<div class="contact-fields-w3ls">
-								<textarea name="message" placeholder="Message..." required=""></textarea>
-							</div>
-							<input type="submit" name="submit" value="Submit">
-						</div>
-					</div>
-
-				</form>
-			</div>
-
-	</div>
+		<div style="color:black">
+		<h4>(M) 0821-2480475 (10:30 am to 5:30 pm) [Except Sunday]</h5>
+		<br>
+		<h4>NIE-BH: <a href="mailto:wardenniebh@nie.ac.in">wardenniebh@nie.ac.in</a></h5>
+		<br>
+		<h4>Yuvika & Yeshvika: <a href="mailto:wardenniegh@nie.ac.in">wardenniegh@nie.ac.in</a></h5>
+		<br>
+		<h4>Caretaker Boy's Hostel - 9886681489</h5>
+		<br>
+		<h4>Caretaker Girl's hostel - 9606241902</h5>
+		</div>
 </section>
 <!-- //contact -->
-
-
-
 
 <!-- js-scripts -->
 
@@ -161,38 +140,3 @@
 
 </body>
 </html>
-
-<?php
-if(isset($_POST['submit'])){
-	/*echo "<script type='text/javascript'>alert('hello')</script>";*/
-	$subject = $_POST['subject'];
-	$message = $_POST['message'];
-	$hostel_name = $_POST['hostel_name'];
-
-    $query7 = "SELECT * FROM Hostel WHERE Hostel_name = '$hostel_name'";
-    $result7 = mysqli_query($conn,$query7);
-    $row7 = mysqli_fetch_assoc($result7);
-    $hostel_id = $row7['Hostel_id'];
-
-    $query6 = "SELECT * FROM Hostel_Manager WHERE Hostel_id = '$hostel_id'";
-    $result6 = mysqli_query($conn,$query6);
-    $row6 = mysqli_fetch_assoc($result6);
-    $hos_man_user = $row6['Hostel_man_id'];
-
-	$roll = $_SESSION['roll'];
-
-    $today_date =  date("Y-m-d");
-    $time = date("h:i A");
-
-	$query = "INSERT INTO Message (sender_id,receiver_id,hostel_id,subject_h,message,msg_date,msg_time) VALUES ('$roll','$hos_man_id','$hostel_id','$subject','$message','$today_date','$time')";
-    $result = mysqli_query($conn,$query);
-    if($result){
-         echo "<script type='text/javascript'>alert('Message sent Successfully!')</script>";
-    }
-    else{
-         echo "<script type='text/javascript'>alert('Error!! Please try again.')</script>";
-   }
-  }
-
-
-?>
